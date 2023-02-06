f2ce3c3854a2b12dba1fb67f284f6529b7fa0f83
Ajeet7080 <ajeetpal450@gmail.com>
2023-02-06 20:48:00 +0530

    completed

--- src/main/java/com/example/redisimplementation/User.java
+++ src/main/java/com/example/redisimplementation/User.java
@@ -1,4 +1,31 @@
 package com.example.redisimplementation;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    /*
+        Agenda of the class
+        there is 2 method to connect redis to Spring boot application
+        1> lettuce method
+        2> Jedis method
+
+        approach ----
+         configuration class
+         lettuce factory
+         redis template
+
+     */
+
+    private int id;
+    private String name;
+    private String country;
+    private String age;
 }
